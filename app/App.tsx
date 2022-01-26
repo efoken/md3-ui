a993c417677a07a83b119ebae4a3d72b29ce4540
Eike Foken <eike.foken@aperto.com>
2022-01-26 16:24:09 +0100

    chore: update app

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,37 @@
 /* eslint-disable unicorn/filename-case */
 import { AppBar } from "@md3-ui/app-bar"
+import { Button } from "@md3-ui/button"
+import { Text } from "@md3-ui/layout"
 import * as React from "react"
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from "react-native-safe-area-context"
 
-const App: React.FC = () => <AppBar />
+const App: React.FC = () => (
+  <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+    <AppBar />
+    <Button>Elevated button</Button>
+    <Button variant="filled">Filled button</Button>
+    <Button variant="tonal">Tonal button</Button>
+    <Button variant="outlined">Outlined button</Button>
+    <Button variant="text">Text button</Button>
+    <Text variant="display-large">Display Large</Text>
+    <Text variant="display-medium">Display Medium</Text>
+    <Text variant="display-small">Display Small</Text>
+    <Text variant="headline-large">Headline Large</Text>
+    <Text variant="headline-medium">Headline Medium</Text>
+    <Text variant="headline-small">Headline Small</Text>
+    <Text variant="title-large">Title Large</Text>
+    <Text variant="title-medium">Title Medium</Text>
+    <Text variant="title-small">Title Small</Text>
+    <Text variant="label-large">Label Large</Text>
+    <Text variant="label-medium">Label Medium</Text>
+    <Text variant="label-small">Label Small</Text>
+    <Text variant="body-large">Body Large</Text>
+    <Text variant="body-medium">Body Medium</Text>
+    <Text variant="body-small">Body Small</Text>
+  </SafeAreaProvider>
+)
 
 export default App
