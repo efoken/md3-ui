32a5a30e4908db0e4e378271620def3ce9f04d6a
Eike Foken <eikefoken@gmail.com>
2022-04-16 10:09:57 +0200

    chore: add github icon

--- packages/icons/stories/icons.stories.tsx
+++ packages/icons/stories/icons.stories.tsx
@@ -1,7 +1,7 @@
 import { IconProps } from "@md3-ui/icon"
-import { Box, Stack, Text } from "@md3-ui/layout"
+import { Box } from "@md3-ui/layout"
 import { Meta, Story } from "@storybook/react"
-import { Add } from "../src/add"
+import * as Md3Icons from "../src"
 
 export default {
   title: "Components/Icons",
@@ -12,10 +12,15 @@ export const Icons: Story<IconProps> = () => (
     style={{
       gridTemplateColumns: `repeat(auto-fill, minmax(8rem, 1fr))`,
     }}
+    sx={{
+      display: "grid" as any,
+      width: "100%",
+    }}
   >
-    <Stack direction="column" spacing={3}>
-      <Add height={40} width={40} />
-      <Text>DirectionsRailway</Text>
-    </Stack>
+    {Object.values(Md3Icons)
+      .filter((Component) => !!Component.displayName)
+      .map((Component) => (
+        <Component height={40} width={40} />
+      ))}
   </Box>
 )
