0aa545fb919e6b1fc07319d4293cc0382f5a23b4
Eike Foken <eikefoken@gmail.com>
2022-06-30 09:00:41 +0200

    chore: silence Storybook warnings about loose mode

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+  ],
   presets: [
     "@babel/preset-typescript",
     "@babel/preset-react",
