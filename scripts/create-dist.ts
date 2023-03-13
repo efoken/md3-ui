89beb219369b6fcf3f7bbe288d34b93841a74a81
Eike Foken <eikefoken@gmail.com>
2023-03-13 15:20:57 +0100

    chore: improve create-dist script

--- scripts/create-dist.ts
+++ scripts/create-dist.ts
@@ -1,13 +1,15 @@
 import glob from "glob"
 import fs from "node:fs"
 import path from "node:path"
-import packageJson from "../package.json"
 
 const rootPath = path.join(__dirname, "..")
 
 async function resolvePackages() {
+  const packageJson = JSON.parse(
+    fs.readFileSync(path.join(rootPath, "package.json")).toString(),
+  )
   const files = await Promise.all(
-    packageJson.preconstruct.packages.map((pattern) =>
+    packageJson.workspaces.map((pattern: string) =>
       glob(pattern, {
         cwd: rootPath,
       }),
@@ -18,7 +20,7 @@ async function resolvePackages() {
 
 function readPackageJson(folder: string) {
   return JSON.parse(
-    fs.readFileSync(path.resolve(rootPath, folder, "package.json")).toString(),
+    fs.readFileSync(path.join(rootPath, folder, "package.json")).toString(),
   )
 }
 
@@ -57,15 +59,15 @@ function writeTypes(outputPath: string, source: string) {
 export default async function main() {
   const packages = await resolvePackages()
   for (const folder of packages) {
-    const localPackageJson = readPackageJson(folder)
+    const packageJson = readPackageJson(folder)
     const { output, source, targets } =
-      localPackageJson["react-native-builder-bob"] ?? {}
+      packageJson["react-native-builder-bob"] ?? {}
 
     if (output && source) {
-      const sourcePath = path.resolve(rootPath, folder, source)
-      const outputPath = path.resolve(rootPath, folder, output)
+      const sourcePath = path.join(rootPath, folder, source)
+      const outputPath = path.join(rootPath, folder, output)
 
-      const target = new Set<string>(targets)
+      const target = new Set<string>(targets.flat())
 
       fs.rmSync(outputPath, { force: true, recursive: true })
       fs.mkdirSync(outputPath)
